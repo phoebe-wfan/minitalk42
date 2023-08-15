a482b0faa22c93d19241e4a4119b775b4ea767db
Wei FAN <wfan@paul-f4Br1s1.clusters.42paris.fr>
2023-08-15 17:18:21 +0200

    normi

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: wfan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 14:47:30 by wfan              #+#    #+#             */
-/*   Updated: 2023/06/02 14:47:32 by wfan             ###   ########.fr       */
+/*   Updated: 2023/08/15 17:14:36 by wfan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@
 void	ft_exit_failure_with_msg(char *message);
 void	send_0(int pid);
 void	send_1(int pid);
-void			ft_putchar_fd(char c, int fd);
-void			ft_putnbr_fd(int n, int fd);
-void			ft_putstr_fd(char *s, int fd);
-size_t			ft_strlen(const char *s);
-int				ft_atoi(const char *str);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+size_t	ft_strlen(const char *s);
+int		ft_atoi(const char *str);
 
 # define GREEN			"\x1b[32m"
 # define RED			"\x1b[31m"
